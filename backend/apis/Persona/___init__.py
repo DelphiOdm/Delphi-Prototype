75d3b26234d35a6acc397fe9e48324e71042685a
VedantMulherkar <vmulherkar@xtsworld.in>
2026-03-17 12:05:40 +0530

    fix: remove pycache and package-lock from git tracking

--- backend/apis/Persona/___init__.py
+++
@@ -1,8 +0,0 @@
-# apis/Persona/__init__.py
-from fastapi import APIRouter
-from .GeneratePersona import router as generate_persona_router
-
-router = APIRouter()
-
-router.include_router(generate_persona_router)
-
